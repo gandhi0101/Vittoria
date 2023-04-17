7ffe658885d050617a47c3d2c9b90c9ced29a177
gandhi0101 <117114451+gandhi0101@users.noreply.github.com>
2023-04-17 09:16:56 -0600

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { SearchComponent } from './footer/search/search.component';
+import { ChatComponent } from './chat/chat.component';
+import { SliderComponent } from './slider/slider.component';
+import { ContactComponent } from './contact/contact.component';
+import { WhyUsComponent } from './why-us/why-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchComponent,
+    ChatComponent,
+    SliderComponent,
+    ContactComponent,
+    WhyUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
