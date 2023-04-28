47d233f5484c6e0cf035e4fedd245346d476db26
gerardo vazquez <gerardo.vaz.galindo@gmail.com>
2023-04-28 10:06:39 -0600

    Implementacion Routing en Navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { WhyUsComponent } from './why-us/why-us.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: WhyUsComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
