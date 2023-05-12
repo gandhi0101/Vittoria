e2904f63f4d48e1fb097e12ffea80c711ee537bc
gandhi0101 <117114451+gandhi0101@users.noreply.github.com>
2023-05-12 14:28:48 -0600

    implementacion de rutas con parametros

--- src/app/why-us/why-us.component.ts
+++ src/app/why-us/why-us.component.ts
@@ -1,4 +1,8 @@
+import { ViewChildren } from '@angular/core';
+import { QueryList } from '@angular/core';
 import { Component } from '@angular/core';
+import { MatExpansionPanel } from '@angular/material/expansion';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-why-us',
@@ -7,4 +11,38 @@ import { Component } from '@angular/core';
 })
 export class WhyUsComponent {
   panelOpenState = false;
+  id: string = '';
+  user: string = '';
+  
+  @ViewChildren(MatExpansionPanel) panels!: QueryList<MatExpansionPanel>;
+
+  ngAfterViewInit() {
+
+    switch (this.id) {
+      case 'gandhi':
+        this.panels.toArray()[0].open();
+        break;
+      case 'juan':
+        this.panels.toArray()[1].open();
+        break;
+      case 'brayan':
+        this.panels.toArray()[2].open();
+        break;
+      case 'gerardo':
+        this.panels.toArray()[3].open();
+        break;
+    }
+  }
+
+
+  constructor(private rout: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.rout.params.subscribe(params => {
+      this.id = params['id'];
+      this.user = `https://github.com/${params['user']}`;
+      console.log(params);
+    });
+  }
+
 }
